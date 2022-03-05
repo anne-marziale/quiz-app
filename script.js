715d73a7e3a7e8bcb23bd1e4654234da58c7144f
anne-marziale <annemarziale@hotmail.com>
2022-03-05 11:07:57 +0100

    first init

---
+++ script.js
@@ -0,0 +1,42 @@
+const quizData = [
+  {
+    question: 'How old is FLorin?',
+    a: '10',
+    b: '17',
+    c: '26',
+    d: '110',
+    correct: 'c',
+  },
+  {
+    question: 'What is the best programming language?',
+    a: 'Java',
+    b: 'C',
+    c: 'Python',
+    d: 'JavaScript',
+    correct: 'a',
+  },
+  {
+    question: 'Who is the president of US?',
+    a: 'Florin Pop',
+    b: 'Donald Trump',
+    c: 'Ivan Saldano',
+    d: 'Obi Wan kanobi',
+    correct: 'b',
+  },
+  {
+    question: 'What does HTML stand for?',
+    a: 'Hypertext Markup Language',
+    b: 'Cascading Style Sheet',
+    c: 'jason Object Notation',
+    d: 'Helocpters Terminals Motorboats Lamborginis',
+    correct: 'a',
+  },
+  {
+    question: 'What year was JavaScript launched?',
+    a: '1996',
+    b: '1995',
+    c: '1994',
+    d: 'none of the above',
+    correct: 'b',
+  },
+];
